81070801f89656148b789c90a929d465ba89e485
dialguiba <dialguiba@gmail.com>
2021-07-14 20:48:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import example from "./images/example.png";
+import "./styles/main.scss";
+
+class Game {
+  name = "Violin Charades";
+}
+const myGame = new Game();
+const p = document.createElement("p");
+p.textContent = `I like ${myGame.name}`;
+
+const heading = document.createElement("h1");
+heading.textContent = "Interesting!";
+
+const app = document.querySelector("#root");
+app.append(heading);
